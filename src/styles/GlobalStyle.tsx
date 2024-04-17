e07c967078351b9e4ca068e53b374d7d1c581843
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-16 19:43:53 -0700

    chore: 프로젝트 세팅

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+import { Global, css } from '@emotion/react';
+import { reset } from '@/styles/reset';
+
+const GlobalStyle = () => (
+	<Global
+		styles={css`
+			${reset}
+			* {
+				font-family: sans-serif !important;
+				box-sizing: border-box;
+			}
+		`}
+	/>
+);
+
+export default GlobalStyle;
